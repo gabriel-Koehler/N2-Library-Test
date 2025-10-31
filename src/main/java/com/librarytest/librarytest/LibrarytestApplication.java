d17e116e0402c210fd6a53c6a2d156c01e64a9fa
gabriel-Koehler <ggaabbrriieellks@gmail.com>
2025-10-31 09:54:26 -0300

    init

---
+++ src/main/java/com/librarytest/librarytest/LibrarytestApplication.java
@@ -0,0 +1,13 @@
+package com.librarytest.librarytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarytestApplication.class, args);
+	}
+
+}
