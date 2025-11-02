4d3e01da81b39a813e3c9246bd228a77b22b470c
gabriel-Koehler <ggaabbrriieellks@gmail.com>
2025-11-02 15:56:48 -0300

    Test with the minimum to be accepted (Verde)

--- src/main/java/com/librarytest/librarytest/Models/Loan.java
+++ src/main/java/com/librarytest/librarytest/Models/Loan.java
@@ -33,10 +33,15 @@ public class Loan {
             Book book,
             EmailService emailService
     ) {
+        if(emailService==null){
+            throw new NullPointerException();
+        }
         if (currentTime.isAfter(this.getDateLoanExpired())) {
             double resultPenalty = 0.0;
-            emailService.sendEmail(user.getEmail(), "Your Loan Date Expired", "Your loan of the book " + book.getTitle() + "is overdue. The fine is" + resultPenalty);
-            return "Penalty is " + resultPenalty;
+
+                emailService.sendEmail(user.getEmail(), "Your Loan Date Expired", "Your loan of the book " + book.getTitle() + "is overdue. The fine is " + resultPenalty);
+                return "Penalty is " + resultPenalty;
+
         }
         return "The loan date has not yet expired";
     }
